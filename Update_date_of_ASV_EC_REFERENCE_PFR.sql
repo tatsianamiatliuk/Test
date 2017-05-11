366d04f39c64c54369960b5408f52ffabc0ebce6
tatsianamiatliuk <tatsiana.miatliuk@gmail.com>
2017-05-12 00:23:43 +0300

    Удалила запрос

--- Update_date_of_ASV_EC_REFERENCE_PFR.sql
+++
@@ -1,5 +0,0 @@
--- Обновить дату создания справки ПФР на дату, меньшую текущей
-
-update ASV_EC_REFERENCE_PFR
-set EC_REFERENCE_PFR_CREATE_DATE = '2016-11-18 00:00:00'
-where EC_REFERENCE_PFR_NUMBER = '506P02160047897';
